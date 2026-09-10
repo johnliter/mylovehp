9a6821748bd027226f89ab9de0cad7b936369cad
John Liter <62905216+johnliter@users.noreply.github.com>
2026-09-09 22:13:52 -0500

    Add offline archive support

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "our-story-v1";
+const CORE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./data/memories.json",
+  "./images/31stbday.jpg",
+  "./images/heart.jpg",
+  "./images/sevenyears.jpg",
+  "./images/mothersday.jpg",
+  "./images/32bday.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
